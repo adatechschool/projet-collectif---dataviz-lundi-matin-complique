74e0839a165d480ac5c66928c01f30a51b43b94a
ArthurGsn <gsn.arthur@icloud.com>
2021-11-18 14:11:46 +0100

    New version
    New version

--- Map.js
+++ Map.js
@@ -41,3 +41,26 @@ const locations = async () => {
   initMap()
 }
 locations()
+
+function getLocation()
+{
+  // Check whether browser supports Geolocation API or not
+  if (navigator.geolocation) { // Supported
+    // To add PositionOptions
+    navigator.geolocation.getCurrentPosition(getPosition);
+  } else { // Not supported
+    alert("Oops! This browser does not support HTML Geolocation.");
+  }
+}
+function getPosition(position)
+{
+  document.getElementById("location").innerHTML =
+      "Latitude: " + position.coords.latitude + "<br>" +
+      "Longitude: " + position.coords.longitude;
+}
+
+if ("geolocation" in navigator) {
+  alert("la géolocalisation est disponible") 
+} else {
+  alert("la géolocalisation n'est pas disponible")
+}
